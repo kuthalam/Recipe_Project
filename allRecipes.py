6ffb3be69d064e3eb7a92860132a525254aebe59
kuthalam <mukundan@utexas.edu>
2021-02-27 17:00:26 -0500

    Now the scraper is getting added.

---
+++ allRecipes.py
@@ -0,0 +1,10 @@
+# A list of all the recipe URLs that we have encountered
+
+recipeURLs = [
+"https://www.allrecipes.com/recipe/20704/romantic-chicken-with-artichokes-and-mushrooms/",
+"https://www.allrecipes.com/recipe/8934/garlic-chicken-stir-fry/",
+"https://www.allrecipes.com/recipe/15181/famous-chicken-francaise/",
+"https://www.allrecipes.com/recipe/232279/one-pot-thai-style-rice-noodles/",
+"https://www.allrecipes.com/recipe/255462/lasagna-flatbread/",
+"https://www.allrecipes.com/recipe/21242/pizza-pasta/"
+]
---
+++ recipeScraper.py
@@ -0,0 +1,122 @@
+### Adapted from the documentation provided here: https://pypi.org/project/requests-html/
+### Written by: Mukundan Kuthalam
+from requests_html import HTMLSession
+import sys
+import json
+import re
+
+def openSession(url):
+    request = None # HTTP Request to scrape the website source
+
+    # Open a request to fetch the HTML content
+    try:
+        session = HTMLSession()
+        request = session.get(url)
+    except:
+        request = None
+
+    return request
+
+def formulateJSON(request):
+    finalJSON = dict() # Result that gets moved to a json
+    # Keys used in the final JSON
+    recipeKey = "recipeName"
+    ingredientsKey = "ingredients"
+    instructionsKey = "instructions"
+
+    # Get the portion of the website source that contains the info we need
+    # Notice this object comes as a string, so we need to parse it to what we want
+    recipeDetails = request.html.find("script", first = True).text
+
+    # Now we get each necessary part of info by traversing the above string
+    finalJSON[recipeKey] = getRecipeName(recipeDetails)
+    finalJSON[ingredientsKey] = getIngredients(recipeDetails)
+    finalJSON[instructionsKey] = getInstructions(recipeDetails)
+
+    return finalJSON
+
+def getRecipeName(recipeInfo):
+    recipeNameTag = "name"
+
+    # Parse out the recipe name
+    nameField = recipeInfo[recipeInfo.index("mainEntityOfPage"):] # Unique key to get us close to the recipe name
+    nameField = nameField[nameField.index(recipeNameTag + "\":") + len(recipeNameTag) + 2:]
+    nameField = nameField[:nameField.index(",")] # From the key name to the first following comma
+
+    # Now get rid of the spaces and quotes
+    nameField = re.sub("\"", "", nameField)[1:]
+
+    return nameField
+
+def getIngredients(recipeInfo):
+    ingredientSiteTag = "recipeIngredient"
+
+    # Parse out the section with all ingredients
+    ingredientString = recipeInfo[recipeInfo.index(ingredientSiteTag):]
+    ingredientString = ingredientString[ingredientString.index("["):ingredientString.index("]") + 1]
+
+    # Construct the list of ingredients
+    ingredientList = []
+    ingredientsLeft = True
+    while ingredientsLeft:
+        # Get the ingredient phrase
+        startIdx = ingredientString.index("\"") + 1
+        stopIdx = ingredientString[startIdx:].index("\"") + startIdx
+        finalIngStr = replaceFractionsWithDecimals(ingredientString[startIdx:stopIdx])
+        ingredientList.append(finalIngStr.strip())
+
+        # Any ingredients left?
+        ingredientString = ingredientString[stopIdx + 1:]
+        try: # Check if there is a quote left in the string
+            ingredientString.index("\"")
+        except:
+            ingredientsLeft = False
+
+    return ingredientList
+
+def getInstructions(recipeInfo):
+    instructionsTag = "recipeInstructions"
+
+    # Parse out just the section with instructions
+    instructionsString = recipeInfo[recipeInfo.index(instructionsTag):]
+    instructionsString = instructionsString[instructionsString.index("["):instructionsString.index("]") + 1]
+
+    # Construct the list of instructions
+    instructionsList = []
+    instructionsLeft = True
+    while instructionsLeft:
+        # Get the instruction text
+        instructionsString = instructionsString[instructionsString.index("text\":") + 8:]
+        stopIdx = instructionsString.index("\\")
+        currentStep = instructionsString[:stopIdx]
+
+        # Since a single step can actually contain multiple steps, some further parsing is done
+        instructionCollection = currentStep.split(".")
+        for elem in instructionCollection:
+            furtherParsedElem = elem.split(";") # There may be semicolons that separate steps
+            for item in furtherParsedElem:
+                if item != "":
+                    item = replaceFractionsWithDecimals(item)
+                    instructionsList.append(item.strip().capitalize())
+
+        # Any instructions left?
+        instructionsString = instructionsString[stopIdx + 1:]
+        try:
+            instructionsString.index("text\":")
+        except:
+            instructionsLeft = False
+
+    return instructionsList
+
+def replaceFractionsWithDecimals(inputStr):
+    # If there are fractions, we can simplify those down to just decimal numbers for ease of use later
+    allFractions = re.findall("\d\/\d", inputStr)
+    for frac in allFractions:
+        numAndDenom = frac.split("/")
+        inputStr = inputStr.replace(frac, str(float(numAndDenom[0])/float(numAndDenom[1])))
+
+    return inputStr
+
+if __name__ == "__main__":
+    request = openSession(sys.argv[1])
+    print(formulateJSON(request))
